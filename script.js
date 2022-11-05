2597f724065fca06756ca7e0433aa5575ac13a39
Steven32323 <stevo323231@gmail.com>
2022-11-05 20:38:21 +0000

    Added dice functionality and setup player scores

--- script.js
+++ script.js
@@ -3,16 +3,21 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.getElementById('current--0');
+const current1El = document.getElementById('current--1');
+
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
 
 // Starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
+let currentScore = 0;
 // Rolling dice functionality
 btnRoll.addEventListener('click', function() {
     // 1. Generate a random dice roll
@@ -21,4 +26,11 @@ btnRoll.addEventListener('click', function() {
     diceEl.classList.remove('hidden');
     diceEl.src = `dice-${dice}.png`;
     // 3. Check for a 1 roll. If true, switch to next player
+    if(dice !== 1) {
+        //add dice to current score
+        currentScore += dice;
+        current0El.textContent = currentScore; //CHANGE LATER
+    } else {
+        //switch to next player
+    }
 })
